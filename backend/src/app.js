de8147d8588e1687ae3dd71f5193158148f13f5a
Anshu Singh <anshusingh0284087@gmail.com>
2026-04-20 16:21:25 +0530

    Backend deploy ready

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-require('dns').setDefaultResultOrder('ipv4first');
+require("dns").setDefaultResultOrder("ipv4first");
 require("dotenv").config();
 
 const express = require("express");
@@ -17,33 +17,29 @@ const groupRoutes = require("./routes/groupRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// DB connection
 connectDB();
 
-// Middlewares
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/groups", groupRoutes);
 
-// Health route
 app.get("/", (req, res) => {
   res.send("ConvoFlow Backend Running 🚀");
 });
 
-// Create HTTP server
 const server = http.createServer(app);
-
-// 🔥 PASS APP INTO SOCKET
 setupSocket(server, app);
 
-// Start server
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
